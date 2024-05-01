8c62b69f6907ac4d4e2da59d4764a6ea8f0ae7fb
Shahriar Fardows <shahriarfardows@gmail.com>
2024-05-02 02:10:08 +0600

    code push

--- src/Root.jsx
+++ src/Root.jsx
@@ -1,8 +1,17 @@
+import { Outlet } from "react-router-dom";
+import Footer from "./Shared/Footer/Footer";
+import Navbar from "./Shared/Navbar/Navbar";
 
 const Root = () => {
     return (
         <div>
-            
+            <Navbar/>
+            <div className="min-h-screen z-50">
+            <Outlet/>
+            </div>
+           <div className="z-10">
+           <Footer/>
+           </div>
         </div>
     );
 };
--- src/main.jsx
+++ src/main.jsx
@@ -2,14 +2,12 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
 
-import {
-  createBrowserRouter,
-  RouterProvider,
-} from "react-router-dom";
+import {RouterProvider,} from "react-router-dom";
+import Routes from './Routes/Routes';
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-
+      <RouterProvider router={Routes} />
   </React.StrictMode>,
 )
