19eb3fc565a58dc02056327f1ac93659985eb44c
jahangir <jahangiralomtuf99@gmail.com>
2024-05-03 21:33:57 +0600

    [FEAT] update Categories

--- src/Home/Categories/Categories.jsx
+++ src/Home/Categories/Categories.jsx
@@ -1,7 +1,5 @@
 /* eslint-disable react/no-unescaped-entities */
 import { Tabs } from "flowbite-react";
-import { HiAdjustments, HiClipboardList } from "react-icons/hi";
-import { MdDashboard } from "react-icons/md";
 
 const Categories = () => {
   return (
@@ -10,42 +8,48 @@ const Categories = () => {
         <Tabs.Item active title="Top Categories">
           <div className="grid grid-cols-4">
             <div className="hover:cursor-pointer hover:underline">
-              <img className="object-center"
+              <img
+                className="object-center"
                 src="https://i.ibb.co/ZW6Xqss/wheels-and-rims-ic-5.jpg"
                 alt=""
               />
               <p className="text-center mt-5">Custom Wheels</p>
             </div>
             <div className="hover:cursor-pointer hover:underline">
-              <img className="object-center"
+              <img
+                className="object-center"
                 src="https://i.ibb.co/ZW6Xqss/wheels-and-rims-ic-5.jpg"
                 alt=""
               />
               <p className="text-center mt-5">Custom Wheels</p>
             </div>
             <div className="hover:cursor-pointer hover:underline">
-              <img className="object-center"
+              <img
+                className="object-center"
                 src="https://i.ibb.co/ZW6Xqss/wheels-and-rims-ic-5.jpg"
                 alt=""
               />
               <p className="text-center mt-5">Custom Wheels</p>
             </div>
             <div className="hover:cursor-pointer hover:underline">
-              <img className="object-center"
+              <img
+                className="object-center"
                 src="https://i.ibb.co/ZW6Xqss/wheels-and-rims-ic-5.jpg"
                 alt=""
               />
               <p className="text-center mt-5">Custom Wheels</p>
             </div>
             <div className="hover:cursor-pointer hover:underline">
-              <img className="object-center"
+              <img
+                className="object-center"
                 src="https://i.ibb.co/ZW6Xqss/wheels-and-rims-ic-5.jpg"
                 alt=""
               />
               <p className="text-center mt-5">Custom Wheels</p>
             </div>
             <div className="hover:cursor-pointer hover:underline">
-              <img className="object-center"
+              <img
+                className="object-center"
                 src="https://i.ibb.co/ZW6Xqss/wheels-and-rims-ic-5.jpg"
                 alt=""
               />
@@ -53,32 +57,57 @@ const Categories = () => {
             </div>
           </div>
         </Tabs.Item>
-        <Tabs.Item title="Dashboard" icon={MdDashboard}>
-          This is{" "}
-          <span className="font-medium text-gray-800 dark:text-white">
-            Dashboard tab's associated content
-          </span>
-          . Clicking another tab will toggle the visibility of this one for the
-          next. The tab JavaScript swaps classes to control the content
-          visibility and styling.
-        </Tabs.Item>
-        <Tabs.Item title="Settings" icon={HiAdjustments}>
-          This is{" "}
-          <span className="font-medium text-gray-800 dark:text-white">
-            Settings tab's associated content
-          </span>
-          . Clicking another tab will toggle the visibility of this one for the
-          next. The tab JavaScript swaps classes to control the content
-          visibility and styling.
-        </Tabs.Item>
-        <Tabs.Item title="Contacts" icon={HiClipboardList}>
-          This is{" "}
-          <span className="font-medium text-gray-800 dark:text-white">
-            Contacts tab's associated content
-          </span>
-          . Clicking another tab will toggle the visibility of this one for the
-          next. The tab JavaScript swaps classes to control the content
-          visibility and styling.
+        <Tabs.Item title="Parts">
+          <div className="grid grid-cols-4">
+            <div className="hover:cursor-pointer hover:underline">
+              <img
+                className="object-center"
+                src="https://i.ibb.co/ZW6Xqss/wheels-and-rims-ic-5.jpg"
+                alt=""
+              />
+              <p className="text-center mt-5">Custom Wheels</p>
+            </div>
+            <div className="hover:cursor-pointer hover:underline">
+              <img
+                className="object-center"
+                src="https://i.ibb.co/ZW6Xqss/wheels-and-rims-ic-5.jpg"
+                alt=""
+              />
+              <p className="text-center mt-5">Custom Wheels</p>
+            </div>
+            <div className="hover:cursor-pointer hover:underline">
+              <img
+                className="object-center"
+                src="https://i.ibb.co/ZW6Xqss/wheels-and-rims-ic-5.jpg"
+                alt=""
+              />
+              <p className="text-center mt-5">Custom Wheels</p>
+            </div>
+            <div className="hover:cursor-pointer hover:underline">
+              <img
+                className="object-center"
+                src="https://i.ibb.co/ZW6Xqss/wheels-and-rims-ic-5.jpg"
+                alt=""
+              />
+              <p className="text-center mt-5">Custom Wheels</p>
+            </div>
+            <div className="hover:cursor-pointer hover:underline">
+              <img
+                className="object-center"
+                src="https://i.ibb.co/ZW6Xqss/wheels-and-rims-ic-5.jpg"
+                alt=""
+              />
+              <p className="text-center mt-5">Custom Wheels</p>
+            </div>
+            <div className="hover:cursor-pointer hover:underline">
+              <img
+                className="object-center"
+                src="https://i.ibb.co/ZW6Xqss/wheels-and-rims-ic-5.jpg"
+                alt=""
+              />
+              <p className="text-center mt-5">Custom Wheels</p>
+            </div>
+          </div>
         </Tabs.Item>
       </Tabs>
     </section>
