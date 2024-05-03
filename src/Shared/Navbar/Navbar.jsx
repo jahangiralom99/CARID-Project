e59edc98ab556ae256990abe7e31931d322a73a8
Shahriar Fardows <shahriarfardows@gmail.com>
2024-05-03 20:08:51 +0600

    navbar code update

--- src/Shared/Navbar/Navbar.jsx
+++ src/Shared/Navbar/Navbar.jsx
@@ -1,10 +1,8 @@
 import logo from '../../../public/logo.png'
 import { useState } from 'react';
-import NavLink from './NavLink';
-import './Drawer.css';
 import './NavDrawer.css'
 import NavDrawer from './NavDrawer';
-import { RxHamburgerMenu } from "react-icons/rx";
+import { NavLink } from 'react-router-dom';
 
 
 const Navbar = () => {
@@ -15,27 +13,64 @@ const Navbar = () => {
         setIsDrawerOpen(!isDrawerOpen);
     };
 
+    const navLink = <>
+        <li>
+            <NavLink to='' className="text-gray-900 md:text-[16px] dark:text-white hover:underline" aria-current="page">Parts</NavLink>
+        </li>
+        <li>
+            <NavLink to='' className="text-gray-900 md:text-[16px] dark:text-white hover:underline">Wheels & Tires</NavLink>
+        </li>
+        <li>
+            <NavLink to='' className="text-gray-900 md:text-[16px] dark:text-white hover:underline">Exterior</NavLink>
+        </li>
+        <li>
+            <NavLink to='' className="text-gray-900 md:text-[16px] dark:text-white hover:underline">Lighting</NavLink>
+        </li>
+        <li>
+            <NavLink to='' className="text-gray-900 md:text-[16px] dark:text-white hover:underline"> Body Parts</NavLink>
+        </li>
+        <li>
+            <NavLink to='' className="text-gray-900 md:text-[16px] dark:text-white hover:underline">Interior</NavLink>
+        </li>
+        <li>
+            <NavLink to='' className="text-gray-900 dark:text-white hover:underline"> Audio & Electronics</NavLink>
+        </li>
+        <li>
+            <NavLink to='' className="text-gray-900 dark:text-white hover:underline"> Automotive Tools</NavLink>
+        </li>
+        <li>
+            <NavLink to='' className="text-gray-900 dark:text-white hover:underline"> Specialty Shops</NavLink>
+        </li>
+    </>
+
     return (
         <div>
             <nav className="bg-white border-gray-200 dark:bg-gray-900">
                 <div className="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl p-4">
                     <div className="flex items-center space-x-3 rtl:space-x-reverse">
-                        <button className="toggle-btn btn btn-square btn-ghost sm:block lg:hidden " onClick={toggleDrawer}>
-                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="inline-block w-5 h-5 stroke-current"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h16"></path></svg>
-                        </button>
-                        <NavDrawer isOpen={isDrawerOpen} toggleDrawer={toggleDrawer} />
+                        <div className='sm:block :hidden '>
+                            <button className="toggle-btn btn btn-square btn-ghost " onClick={toggleDrawer}>
+                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="inline-block w-5 h-5 stroke-current"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h16"></path></svg>
+                            </button>
+                            <NavDrawer isOpen={isDrawerOpen} toggleDrawer={toggleDrawer} />
+                        </div>
                         <img src={logo} className="h-8" alt="Logo" />
                         <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white"></span>
+                    </div>
+                    <div>
+                        
                     </div>
                     <div className="flex items-center space-x-6 rtl:space-x-reverse">
                         <a to='' className="text-sm  text-blue-600 dark:text-blue-500 hover:underline">Login</a>
                     </div>
                 </div>
             </nav>
-            <nav className="bg-gray-50 dark:bg-gray-700 sm:hidden md:block ">
+            <nav className="bg-gray-50 dark:bg-gray-700 hidden lg:block ">
                 <div className="max-w-screen-xl px-4 py-3 mx-auto">
                     <div className="flex items-center justify-around">
-                        <NavLink />
+                        <ul className="  lg:flex lg:flex-row lg:font-medium  lg:gap-[1.5rem] lg:mt-0 lg:space-x-8 lg:rtl:space-x-reverse lg:text-sm  ">
+                            {navLink}
+                        </ul>
                     </div>
                 </div>
             </nav>
