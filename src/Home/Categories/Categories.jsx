d21a87d81809720c99a35ad90ff18c165831b678
jahangir <jahangiralomtuf99@gmail.com>
2024-05-05 20:45:07 +0600

    [UPDATE] added Category in MONGOBD

--- src/Home/Categories/Categories.jsx
+++ src/Home/Categories/Categories.jsx
@@ -1,121 +1,48 @@
 /* eslint-disable react/no-unescaped-entities */
 
-import { useEffect, useState } from "react";
 import { Tab, TabList, TabPanel, Tabs } from "react-tabs";
+import CategoryTab from "../../Components/CategoryTab/CategoryTab";
 
 const Categories = () => {
-  const [category, setCategory] = useState([]);
-
-  useEffect(() => {
-    fetch("http://localhost:5000/api/v1/category")
-      .then((res) => res.json())
-      .then((data) => setCategory(data));
-  }, []);
-
-  console.log(category);
 
   return (
-    <section className="max-w-screen-xl mx-auto mt-12 px-3">
-
+    <section className="max-w-screen-xl mx-auto mt-12 px-4">
       <div>
         <Tabs>
-          <TabList>
-            <Tab>Mario</Tab>
-            <Tab>Luigi</Tab>
-            <Tab>Peach</Tab>
-            <Tab>Yoshi</Tab>
-            <Tab>Toad</Tab>
+          <TabList className="font-bold border-b-2 border-black gap-12">
+            <Tab>Parts</Tab>
+            <Tab>Wheels & Tires</Tab>
+            <Tab>Exterior</Tab>
+            <Tab>Lighting</Tab>
+            <Tab>Body</Tab>
+            <Tab>Interior</Tab>
+            <Tab>Audio</Tab>
+            <Tab>Automotive Tools</Tab>
           </TabList>
-
+          {/* Category content */}
           <TabPanel>
-           
+            <CategoryTab category="parts"></CategoryTab>
           </TabPanel>
           <TabPanel>
-            <p>
-              <b>Luigi</b> (
-              <i>Japanese: ルイージ Hepburn: Ruīji, [ɾɯ.iː.dʑi̥]</i>) (
-              <i>English: /luˈiːdʒi/; Italian: [luˈiːdʒi]</i>) is a fictional
-              character featured in video games and related media released by
-              Nintendo. Created by prominent game designer Shigeru Miyamoto,
-              Luigi is portrayed as the slightly younger but taller fraternal
-              twin brother of Nintendo's mascot Mario, and appears in many games
-              throughout the Mario franchise, often as a sidekick to his
-              brother.
-            </p>
-            <p>
-              Source:{" "}
-              <a href="https://en.wikipedia.org/wiki/Luigi" target="_blank">
-                Wikipedia
-              </a>
-            </p>
+            <CategoryTab category="wheels_tires"></CategoryTab>
           </TabPanel>
           <TabPanel>
-            <p>
-              <b>Princess Peach</b> (
-              <i>Japanese: ピーチ姫 Hepburn: Pīchi-hime, [piː.tɕi̥ çi̥.me]</i>) is
-              a character in Nintendo's Mario franchise. Originally created by
-              Shigeru Miyamoto, Peach is the princess of the fictional Mushroom
-              Kingdom, which is constantly under attack by Bowser. She often
-              plays the damsel in distress role within the series and is the
-              lead female. She is often portrayed as Mario's love interest and
-              has appeared in Super Princess Peach, where she is the main
-              playable character.
-            </p>
-            <p>
-              Source:{" "}
-              <a
-                href="https://en.wikipedia.org/wiki/Princess_Peach"
-                target="_blank"
-              >
-                Wikipedia
-              </a>
-            </p>
+            <CategoryTab category="exterior"></CategoryTab>
           </TabPanel>
           <TabPanel>
-            <p>
-              <b>Yoshi</b> (<i>ヨッシー Yosshī, [joɕ.ɕiː]</i>) (
-              <i>English: /ˈjoʊʃi/ or /ˈjɒʃi/</i>), once romanized as Yossy, is
-              a fictional anthropomorphic dinosaur who appears in video games
-              published by Nintendo. Yoshi debuted in Super Mario World (1990)
-              on the Super Nintendo Entertainment System as Mario and Luigi's
-              sidekick. Yoshi later starred in platform and puzzle games,
-              including Super Mario World 2: Yoshi's Island, Yoshi's Story and
-              Yoshi's Woolly World. Yoshi also appears in many of the Mario
-              spin-off games, including Mario Party and Mario Kart, various
-              Mario sports games, and Nintendo's crossover fighting game series
-              Super Smash Bros. Yoshi belongs to the species of the same name,
-              which is characterized by their variety of colors.
-            </p>
-            <p>
-              Source:{" "}
-              <a href="https://en.wikipedia.org/wiki/Yoshi" target="_blank">
-                Wikipedia
-              </a>
-            </p>
+            <CategoryTab category="lighting"></CategoryTab>
           </TabPanel>
           <TabPanel>
-            <p>
-              <b>Toad</b> (<i>Japanese: キノピオ Hepburn: Kinopio</i>) is a
-              fictional character who primarily appears in Nintendo's Mario
-              franchise. Created by Japanese video game designer Shigeru
-              Miyamoto, he is portrayed as a citizen of the Mushroom Kingdom and
-              is one of Princess Peach's most loyal attendants; constantly
-              working on her behalf. He is usually seen as a non-player
-              character (NPC) who provides assistance to Mario and his friends
-              in most games, but there are times when Toad(s) takes center stage
-              and appears as a protagonist, as seen in Super Mario Bros. 2,
-              Wario's Woods, Super Mario 3D World, and Captain Toad: Treasure
-              Tracker.
-            </p>
-            <p>
-              Source:{" "}
-              <a
-                href="https://en.wikipedia.org/wiki/Toad_(Nintendo)"
-                target="_blank"
-              >
-                Wikipedia
-              </a>
-            </p>
+            <CategoryTab category="body"></CategoryTab>
+          </TabPanel>
+          <TabPanel>
+            <CategoryTab category="interior"></CategoryTab>
+          </TabPanel>
+          <TabPanel>
+            <CategoryTab category="audio"></CategoryTab>
+          </TabPanel>
+          <TabPanel>
+            <CategoryTab category="automotive_tools"></CategoryTab>
           </TabPanel>
         </Tabs>
       </div>
@@ -124,3 +51,5 @@ const Categories = () => {
 };
 
 export default Categories;
+
+//
