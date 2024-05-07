e7ec7d635a813f6bfccdfa9444173a07c006feb2
Shahriar Fardows <shahriarfardows@gmail.com>
2024-05-07 13:47:11 +0600

    all code update

--- src/Firebase/Auth/AuthProvider.jsx
+++ src/Firebase/Auth/AuthProvider.jsx
@@ -1,5 +1,6 @@
-import { getAuth } from "firebase/auth";
-import {  createContext } from "react";
+/* eslint-disable react/prop-types */
+import { createUserWithEmailAndPassword, getAuth, onAuthStateChanged, signInWithEmailAndPassword, signOut } from "firebase/auth";
+import { createContext, useEffect, useState } from "react";
 import app from "../Firebase";
 
 export const AuthContext = createContext();
@@ -7,19 +8,57 @@ export const AuthContext = createContext();
 const auth = getAuth(app);
 
 
-const AuthProvider = ({Children}) => {
+const AuthProvider = ({ Children }) => {
+    const [user, setUser] = useState(null);
+    const [loading, setLoading] = useState(true)
+
+    useEffect(() => {
+        const unsubscribe = onAuthStateChanged(auth, user => {
+            setUser(user);
+            setLoading(false)
+        })
+        return () => {
+            return unsubscribe();
+        };
+
+    }, [])
+
+    // user sign up code 
+
+    const createUser = (email, password) => {
+        setLoading(true)
+        return createUserWithEmailAndPassword(auth, email, password);
+    }
+
+    // user login code 
+
+    const loginUser = (email, password) => {
+        setLoading(true)
+        return signInWithEmailAndPassword(email, password);
+    }
+
+    // user log Out code 
+
+    const LogOut = () => {
+        return signOut(auth) ;
+    }
+
 
 
 
     const authInfo = {
         user,
-        
+        loading,
+        createUser,
+        loginUser,
+        LogOut
+
     }
 
 
 
     return (
-        <AuthContext.Provider  value={authInfo}>
+        <AuthContext.Provider value={authInfo}>
             {Children}
         </AuthContext.Provider>
     );
